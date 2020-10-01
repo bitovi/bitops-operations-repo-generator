bb28e9ee7b109ec18f4d3c5fe255cb32cb5b5a46
Connor Graham <connor@bitovi.com>
2020-10-01 12:33:27 -0400

    yeoman bitops generator

---
+++ README.md
@@ -0,0 +1,33 @@
+# generator-bitops [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Skaffolding for an operations repo built to work with https://github.com/bitovi/bitops
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-bitops [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-bitops
+```
+
+Then generate your new project:
+
+```bash
+yo bitops
+```
+
+### Installing from source
+
+Instead of installing `generator-bitops` using npm, you can clone this repo and run `npm link`.
+
+## License
+
+MIT © [Bitovi](https://www.bitovi.com/)
+
+
+[npm-image]: https://badge.fury.io/js/generator-bitops.svg
+[npm-url]: https://npmjs.org/package/generator-bitops
+[travis-image]: https://travis-ci.com/bitovi/generator-bitops.svg?branch=master
+[travis-url]: https://travis-ci.com/bitovi/generator-bitops
+[daviddm-image]: https://david-dm.org/bitovi/generator-bitops.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/bitovi/generator-bitops
---
+++ generators/app/index.js
@@ -0,0 +1,88 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(`Welcome to the ${chalk.red('bitops operations repository')} generator!`)
+    );
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'appName',
+        message: 'What is the name of your application (e.g. serviceA or production)?',
+        default: 'my-app'
+      },
+      {
+        type: 'confirm',
+        name: 'terraform',
+        message: 'Does your application need terraform?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'cloudformation',
+        message: 'Does your application need cloudformation?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'ansible',
+        message: 'Does your application need ansible?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'helm',
+        message: 'Does your application need helm?',
+        default: true
+      },
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    var env = this.props.appName;
+
+    if(this.props.terraform){
+      this.fs.copyTpl(
+        this.templatePath('terraform/**/*'),
+        this.destinationPath(`${this.destinationRoot()}/${env}/terraform`)
+      )
+    }
+
+    if(this.props.cloudformation){
+      this.fs.copyTpl(
+        this.templatePath('cloudformation/**/*'),
+        this.destinationPath(`${this.destinationRoot()}/${env}/cloudformation`)
+      )
+    }
+
+    if(this.props.ansible){
+      this.fs.copyTpl(
+        this.templatePath('ansible/**/*'),
+        this.destinationPath(`${this.destinationRoot()}/${env}/ansible`)
+      )
+    }
+
+    if(this.props.helm){
+      this.fs.copyTpl(
+        this.templatePath('helm/**/*'),
+        this.destinationPath(`${this.destinationRoot()}/${env}/helm`)
+      )
+    }
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath(`${this.destinationRoot()}/README.md`), {
+        env: env
+      }
+    )
+  }
+
+};
